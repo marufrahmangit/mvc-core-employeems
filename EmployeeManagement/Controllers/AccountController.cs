1bace4bb2b1c193dcbf30d7a2e9f2efdd00ad3db
Munem Maruf <primegh@live.com>
2019-07-28 11:35:27 +0600

    Access Denied method and View added, Manage Roles navigation added

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -113,5 +113,12 @@ namespace EmployeeManagement.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
